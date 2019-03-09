d8d0b59a7ee67cc31737b05983d5df2a50a1c7c4
Cody Yantis <cyantis@gmail.com>
2019-03-09 22:09:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
 def alphabetize(arr)
   e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|a| a.split("").collect |c| e_alphabet.index(c)}
+  arr.sort_by {|a| a.split("").collect {|c| e_alphabet.index(c)}}
 end
